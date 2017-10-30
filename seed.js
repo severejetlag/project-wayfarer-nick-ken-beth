bba81e11b3d5dfb585b6646c449219346b67e1a4
brwitten <bethrwitten@gmail.com>
2017-10-30 13:38:51 -0700

    added initial folders

---
+++ seed.js
@@ -0,0 +1,19 @@
+var db = require("./models");
+
+var newUsers = [{
+  username: "NotNickLee"
+},{
+  username: "NotKenDong"
+},{
+  username: "NotBethWitten"
+}];
+
+var newCity = [{
+  cityName: "San Francisco"
+}];
+
+var newPost = [{
+  postTitle: "Fog, fog city"
+},{
+  postContent: "Occupy raw denim lumbersexual marfa umami, poke whatever chia synth tbh. Slow-carb pork belly pickled single-origin coffee. Pug affogato farm-to-table retro put a bird on it thundercats, dreamcatcher drinking vinegar artisan man bun quinoa banh mi. Pop-up bespoke meggings photo booth, activated charcoal neutra aesthetic gluten-free sustainable cred sartorial four loko. Cronut small batch viral swag YOLO before they sold out shabby chic asymmetrical vice seitan godard readymade ugh franzen. Kinfolk poke chillwave neutra banjo flexitarian bespoke succulents hot chicken. Brooklyn iceland helvetica pickled try-hard wolf cold-pressed distillery sriracha freegan copper mug aesthetic."
+}];
