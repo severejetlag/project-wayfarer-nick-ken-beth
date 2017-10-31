e8bd66af5aa2efa055d0c8a1aed5ad8cef312f19
severejetlag <nhl1013@gmail.com>
2017-10-31 12:29:38 -0700

    Getting basic React structure built

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import {browserHistory} from 'react-router';
 import {Link} from 'react-router';
+import HeaderContainer from './containers/headerContainer'
 
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      username: '', 
+      password: '',
+      isAuthed: false
+    }
+  }
   render() {
     return (
       <div className="App">
-        <p>TEST</p>
+        <div className='container'>
+        <HeaderContainer/>
+        </div> 
       </div>
     );
   }
