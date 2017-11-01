346f9e6ae20918d90ee5744453e5399ba8ae7d01
brwitten <bethrwitten@gmail.com>
2017-11-01 11:53:00 -0700

    added initial profile image upload

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
 
-  
+  console.log("linked!");
+
+  var singleWidget = uploadcare.SingleWidget('[role=uploadcare-uploader]');
+
+  singleWidget.onUploadComplete(function(info) {
+      console.log(info);
+      console.log(info.originalUrl);
+      console.log("upload complete");
+      $('.personalIcon').attr("src",info.originalUrl);
+  });
+
+
+
 });
