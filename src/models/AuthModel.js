1493e6d679213aa140743e24dc2aee05e83e4c3b
brwitten <bethrwitten@gmail.com>
2017-11-01 20:18:39 -0700

    added new post button to profile page

--- src/models/AuthModel.js
+++ src/models/AuthModel.js
@@ -1,18 +1,18 @@
-import axios from 'axios'
-
-class AuthModel {
-  	static login(authData){
-    	let request = axios.post(`http://localhost:3001/login`, authData)
-    	return request
-  	}
-  	static signup(authData){
-  		let request = axios.post('http://localhost:3001/signup', authData)
-  		return request
-  	}
-  	static logout(){
-  		let request	= axios.get('http://localhost:3001/logout')
-  		return request
-  	}
-}
-
-export default AuthModel
+// import axios from 'axios'
+//
+// class AuthModel {
+//   	static login(authData){
+//     	let request = axios.post(`http://localhost:3001/login`, authData)
+//     	return request
+//   	}
+//   	static signup(authData){
+//   		let request = axios.post('http://localhost:3001/signup', authData)
+//   		return request
+//   	}
+//   	static logout(){
+//   		let request	= axios.get('http://localhost:3001/logout')
+//   		return request
+//   	}
+// }
+//
+// export default AuthModel
